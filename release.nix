2c53e39e5d8691221805e648a31b42697f81ccb8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-11-13 15:41:05 -0500

    Build the site, and use GitHub Actions

---
+++ release.nix
@@ -0,0 +1,35 @@
+let
+  # "Nixpkgs-free" fetchFromGitHub... we use that repo's pin on nixpkgs instead.
+  # This is expected to be run in a CI/CD context.
+  gh = {branch, repo, owner}:
+    builtins.fetchTarball "https://github.com/${owner}/${repo}/archive/${branch}.tar.gz"
+  ;
+  mobile-nixos = gh {
+    branch = "master";
+    owner = "NixOS";
+    repo = "mobile-nixos";
+  };
+  pkgs = import "${mobile-nixos}/doc/pkgs.nix";
+  site = import "${mobile-nixos}/doc" { inherit pkgs; };
+  inherit (pkgs.lib) optionalString;
+
+  inherit (pkgs) rsync;
+
+  CNAME = "mobile.nixos.org";
+in
+
+  # Here we patch over the docs to insert the website contents.
+  site.overrideAttrs(old: {
+    buildInputs = old.buildInputs ++ [
+      rsync
+    ];
+
+    postPatch = ''
+      echo " :: Adding the website data to the docs source."
+      rsync --recursive --verbose ${./.}/website/ ./
+    '';
+
+    postFixup = optionalString (CNAME != null) ''
+      echo '${CNAME}' >> $out/CNAME
+    '';
+  })
